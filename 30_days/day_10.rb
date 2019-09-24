8d3968260b0e9777582ab3fb9ca61faa526605c2
hanifbg <hanifbg48@gmail.com>
2019-09-24 10:51:57 +0700

    day 10 add other method

--- 30_days/day_10.rb
+++ 30_days/day_10.rb
@@ -5,27 +5,43 @@ require 'stringio'
 
 n = gets.to_i
 @result = []
-
 while n > 0
 	sisa = n % 2
 	n = n / 2
 	@result << sisa
 end
 
+starting = Time.now
 i = 0
 highest = 0
-@result.reverse.each_with_index do |a, index|
-	i += 1 if a == 1
+
+##first method
+@result.reverse.each do |a|
+	if a == 1
+		i += 1
+		temp = i
+		highest = temp if temp >= highest
+	end
 	if a == 0
 		temp = i
 		i = 0
 		highest = temp if temp >= highest
 	end
-  if index == @result.reverse.size - 1
-		temp = i
-		i = 0
-		highest = temp if temp >= highest
-  end
 end
+
+##second method
+# @result.reverse.each_with_index do |a, index|
+# 	i += 1 if a == 1
+# 	if a == 0
+# 		temp = i
+# 		i = 0
+# 		highest = temp if temp >= highest
+# 	end
+#   if index == @result.reverse.size - 1
+# 		temp = i
+# 		i = 0
+# 		highest = temp if temp >= highest
+#   end
+# end
 
 print highest
